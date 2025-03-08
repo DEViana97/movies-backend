222eed5933d4ab56172c9813ae136613a3237001
Matheus Viana <mateusviana97@gmail.com>
2025-03-08 12:30:42 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Configuração de CORS
+  app.enableCors({
+    origin: '*', // Permite todas as origens (para teste); ajuste para produção
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true, // Se precisar de cookies ou autenticação baseada em credenciais
+  });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
